fca4ff6739a92782cbc775deafa8ae9e1d435cec
Vaibhav Sharma <vaibhav.cruciyo@gmail.com>
2021-08-03 20:18:17 +0530

    initial commit

---
+++ add.java
@@ -0,0 +1,9 @@
+import java.util.*;
+
+class abc {
+    public static void main(String args[]) {
+        int a = 10;
+        int b = 20;
+        System.out.println(a + b);
+    }
+}
---
+++ multi.java
@@ -0,0 +1,7 @@
+class abc {
+    public static void main(String args[]) {
+        int a = 10;
+        int b = 20;
+        System.out.println(a * b);
+    }
+}
