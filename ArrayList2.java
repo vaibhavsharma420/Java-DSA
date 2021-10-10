79e3702cda444f89c9479b7a3ac268cdde55e52d
Vaibhav Sharma <73660640+vaibhavsharma420@users.noreply.github.com>
2021-10-10 15:50:57 +0530

    commit

---
+++ ArrayList2.java
@@ -0,0 +1,16 @@
+import java.util.*;
+import java.util.ArrayList;
+import java.util.Collections;
+
+public class Main {
+    public static void main(String[] args) {
+        // Write your code here
+        ArrayList<Integer> na = new ArrayList<>();
+        na.add(7);
+        na.add(8);
+        na.add(4);
+        na.add(2);
+        int h = na.get(2);
+        System.out.println(h);
+    }
+}
