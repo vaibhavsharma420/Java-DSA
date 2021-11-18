3a1dd84283d24e621d65274756e98bfc3bc812bb
Vaibhav Sharma <73660640+vaibhavsharma420@users.noreply.github.com>
2021-11-18 12:22:48 +0530

    commit

--- mail.java
+++ mail.java
@@ -1,1 +1,17 @@
-vaibhav.cruciyo@gmail.com
+import java.util.*;
+public class Main
+{
+	public static void main(String[] args) {
+		HashMap<String, Integer> ser= new HashMap<>();
+		ser.put("hen",2);
+		ser.put("yellow",3);
+		ser.put("three",4);
+		System.out.println(ser);
+		ser.remove("hen");
+		System.out.println(ser);
+		int g=ser.get("yellow");
+		System.out.println(g);
+		boolean y=ser.containsKey("three");
+		System.out.println(y);
+	}
+}
